87df8e21bdc0967f1918daf7ddcb31d6b8a7c254
dinhog17 <dinhog1706@gmail.com>
2025-11-03 12:55:38 -0300

    Criando folders e inicial page

---
+++ tf-poo/src/main/java/pages/inicial.java
@@ -0,0 +1,17 @@
+package pages;
+
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.router.Route;
+import com.vaadin.flow.router.RouterLink;
+
+public class inicial extends VerticalLayout {
+    RouterLink entrada;
+    RouterLink saida;
+    RouterLink pagamento;
+    RouterLink informacao;
+    RouterLink editarPlaca;
+
+    public inicial(){
+
+    }
+}
